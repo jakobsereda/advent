872e4181b4ba8f527ae134ad8544b49a68815a99
jakob <138621739+jakobsereda@users.noreply.github.com>
2024-11-06 10:40:08 -0800

    feat: started day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::BufReader;
+use std::fs::{
+    self, 
+    File
+};
+
+fn main() -> io::Result<()> {
+    let data = File::create("data/data.txt")?;
+    let buf = BufReader::new(data);
+
+    
+
+    Ok(())
 }
