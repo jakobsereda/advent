9f5cbc3384464d0062ca707394a67ec37770348b
jakob <138621739+jakobsereda@users.noreply.github.com>
2024-11-08 16:02:08 -0800

    feat: refactored day2-part1
    ccode is now more declarative and has less nesting (hooray)

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{
     self,
@@ -5,66 +6,50 @@ use std::io::{
     BufReader
 };
 
-const MAX_RED: i32 = 12;
-const MAX_BLUE: i32 = 14;
-const MAX_GREEN: i32 = 13;
+const MAX_NUMS: &[(&str, i32)] = &[
+    ("red", 12),
+    ("blue", 14),
+    ("green", 13),
+];
 
 fn main() -> io::Result<()> {
     let data = File::open("data/data.txt")?;
     let buf = BufReader::new(data);
 
+    let map: HashMap<&str, i32> = MAX_NUMS.iter().cloned().collect();
     let mut sum = 0;
+
     for line in buf.lines() {
         let line = line?;
-        
-        let id = line.split_whitespace()
-            .nth(1)
-            .and_then(|n| n.trim_end_matches(':').parse::<i32>().ok())
-            .unwrap_or_default();
-
-        let game= line.split(':')
-            .last()
-            .map(|s| s.trim_end().split(';').collect::<Vec<&str>>())
-            .unwrap_or_default();
-
-        let mut valid_line = true;
-
-        for elm in game {
-            let sets = elm.split(',')
-                .map(|s| s.trim_end())
-                .collect::<Vec<&str>>();
-
-            let mut valid_game = true;
-
-            for set in sets {
-                let (n, color) = {
-                    let mut pair = set.split_whitespace();
-                    let n = pair.next().and_then(|n| n.parse::<i32>().ok())
-                        .unwrap_or_default();
-                    let color = pair.next()
-                        .unwrap_or_default();
-                    (n, color)
-                };
-                
-                match color {
-                    "red" => if n > MAX_RED { valid_game = false },
-                    "blue" => if n > MAX_BLUE { valid_game = false },
-                    "green" => if n > MAX_GREEN { valid_game = false },
-                    _ => {}
-                }
-            }
-
-            if !valid_game { 
-                valid_line = false;
-            }
-        }
-
-        if valid_line {
-            sum += id;
+        if let Some(id) = get_id(&line) {
+            if is_valid_game(&line, &map) { sum += id }
         }
     }
 
     println!("{}", sum);
 
     Ok(())
+}
+
+fn get_id(line: &str) -> Option<i32> {
+    line.split_whitespace()
+        .nth(1)
+        .and_then(|n| n.trim_end_matches(':').parse::<i32>().ok())
+}
+
+fn is_valid_game(line: &str, map: &HashMap<&str, i32>) -> bool {
+    line.split(':')
+        .last()
+        .map(|game| game.split(';').all(|set| is_valid_set(set, &map)))
+        .unwrap_or(false)
+}
+
+fn is_valid_set(set: &str, map: &HashMap<&str, i32>) -> bool {
+    set.split(',')
+        .all(|cubes| {
+            let mut pair = cubes.split_whitespace();
+            let n = pair.next().and_then(|n| n.parse::<i32>().ok()).unwrap_or(0);
+            let color = pair.next().unwrap_or("");
+            map.get(color).map_or(true, |&max| n <= max)
+        })
 }
