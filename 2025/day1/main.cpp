0aa29046602d172a5bb88b5ae7515a63e204a9e8
jakob <138621739+jakobsereda@users.noreply.github.com>
2025-11-30 22:36:36 -0800

    feat: day 1 2025

--- 2025/day1/main.cpp
+++ 2025/day1/main.cpp
@@ -4,8 +4,44 @@ int main(int argc, char *argv[])
 {
     vector<string> lines = read_lines("data/data.txt");
 
-    for (string line : lines)
-        cout << line << '\n';
+    i32 cnt = 0;
+    i32 cnt2 = 0;
+    i32 dial = 50;
+
+    for (string line : lines) {
+        char dir = line[0];
+        i32 ticks = stoi(line.substr(1));
+
+        // ==== part two ====
+
+        i32 dist;
+        if (dir == 'R') {
+            dist = (100 - dial) % 100;
+        } else {
+            dist = dial % 100;
+        }
+
+        if (dist == 0)
+            dist = 100;
+
+        if (dist <= ticks)
+            cnt2 += 1 + (ticks - dist) / 100;
+
+        // ==================
+
+        if (dir == 'R') {
+            dial = (dial + ticks) % 100;
+        } else {
+            dial = (dial - ticks) % 100;
+            if (dial < 0) dial += 100;
+        }
+
+        if (dial == 0)
+            cnt++;
+    }
+    
+    cout << "Part one: " << cnt << '\n';
+    cout << "Part two: " << cnt2 << '\n';
 
     return 0;
 }
