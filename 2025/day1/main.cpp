75ac74179b8ced846c2fe46da4e6e7a3b2ba08ec
jakob <138621739+jakobsereda@users.noreply.github.com>
2025-11-30 16:06:53 -0800

    feat: add aoc header

--- 2025/day1/main.cpp
+++ 2025/day1/main.cpp
@@ -1,24 +1,11 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-typedef uint8_t  u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
+#include "aoc.hpp"
 
 int main(int argc, char *argv[])
 {
-    ifstream file("data/data.txt");
-    string   s;
-    string   data;
+    vector<string> lines = read_lines("data/data.txt");
 
-    while (getline(file, s)) {
-        data += s;
-        data.push_back('\n');
-    }
+    for (string line : lines)
+        cout << line << '\n';
 
-    cout << data;
-    
     return 0;
 }
