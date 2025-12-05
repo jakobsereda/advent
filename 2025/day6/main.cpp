9dd5a31d74e4782a9d2f18233547e07951c1526f
jakob <138621739+jakobsereda@users.noreply.github.com>
2025-12-04 22:49:55 -0800

    refactor: use symlinks for makefile

---
+++ 2025/day6/main.cpp
@@ -0,0 +1,11 @@
+#include "aoc.hpp"
+
+int main(void)
+{
+    vector<string> lines = read_lines("data/data.txt");
+
+    cout << "Part one: " << '\n';
+    cout << "Part two: " << '\n';
+
+    return 0;
+}
