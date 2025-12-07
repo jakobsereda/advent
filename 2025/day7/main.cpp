a0383e2047bd17e04c5fc170f372a5c744f5bf93
jakob <138621739+jakobsereda@users.noreply.github.com>
2025-12-07 12:19:40 -0800

    feat: day 7 2025 part 2

--- 2025/day7/main.cpp
+++ 2025/day7/main.cpp
@@ -1,5 +1,28 @@
 #include "aoc.hpp"
 
+map<pair<size_t, size_t>, i64> dp;
+
+i64 find_timelines(size_t idx, size_t depth, const vector<vector<char>> &g)
+{
+    // if (idx >= g[0].size())
+    //     return 0;
+    while (g[depth][idx] != '^') {
+        if (depth == g.size() - 1)
+            return 1;
+        depth++;
+    }
+    auto key = make_pair(idx, depth);
+    if (dp.contains(key))
+        return dp[key];
+    i64 l = 0;
+    i64 r = 0;
+    if (idx > 0)
+        l = find_timelines(idx - 1, depth + 1, g);
+    if (idx + 1 < g[0].size())
+        r = find_timelines(idx + 1, depth + 1, g);
+    return dp[key] = l + r;
+}
+
 int main(void)
 {
     vector<string> lines = read_lines("data/data.txt");
@@ -9,10 +32,14 @@ int main(void)
         for (size_t j = 0; j < lines[0].length(); j++)
             grid[i][j] = lines[i][j];
     
+    size_t s_idx = 0;
     set<size_t> lanes;
-    for (size_t i = 0; i < grid[0].size(); i++)
-        if (grid[0][i] == 'S')
+    for (size_t i = 0; i < grid[0].size(); i++) {
+        if (grid[0][i] == 'S') {
             lanes.insert(i);
+            s_idx = i;
+        }
+    }
 
     i64 sum = 0;
     for (size_t i = 1; i < grid.size(); i++) {
@@ -29,7 +56,11 @@ int main(void)
         }
     }
 
-    i64 sum2 = 0;
+    // ==== part two ====
+
+    i64 sum2 = find_timelines(s_idx, 1, grid);
+
+    // ==================
 
     cout << "Part one: " << sum << '\n';
     cout << "Part two: " << sum2 << "\n";
